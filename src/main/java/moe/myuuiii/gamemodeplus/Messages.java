70f51183ebef6fd8b2d2b05160240fafd788c451
“whiteONred” <whiteonred@vivaldi.net>
2023-12-07 13:53:25 +0300

    Translation of messages into Belarusian

--- src/main/java/moe/myuuiii/gamemodeplus/Messages.java
+++ src/main/java/moe/myuuiii/gamemodeplus/Messages.java
@@ -1,28 +1,26 @@
 package moe.myuuiii.gamemodeplus;
 
-import org.checkerframework.checker.units.qual.Prefix;
-
 import net.md_5.bungee.api.ChatColor;
 
 public class Messages {
-	public static final String Prefix = ChatColor.GRAY + "[" + ChatColor.GREEN + "Gamemode+" + ChatColor.GRAY + "]"
+	public static final String Prefix = ChatColor.WHITE + "[" + ChatColor.WHITE + "Сервер" + ChatColor.WHITE + "]"
 			+ " ";
-	public static final String NotAPlayer = Prefix + "You can only execute this command as a player";
-	public static final String NotAllowedToUsePlugin = Prefix + "You are not allowed to make use of this plugin";
-	public static final String GamemodeNotRecognized = Prefix + "The gamemode you provided was not recognized";
-	public static final String AlreadyInGamemode = Prefix + "You are already in that gamemode";
-	public static final String PlayerNotFound = Prefix + "That player could not be found";
+	public static final String NotAPlayer = Prefix + "Вы павінны выконваць каманду як гулец.";
+	public static final String NotAllowedToUsePlugin = Prefix + "Вы не маеце дазволу пераключаць рэжым гульні.";
+	public static final String GamemodeNotRecognized = Prefix + "Няправільны рэжым гульні.";
+	public static final String AlreadyInGamemode = Prefix + "Вы ўжо у гэтым рэжыме гульні.";
+	public static final String PlayerNotFound = Prefix + "Гулец не знойдзены";
 	public static final String NotAllowedToChangeForOthers = Prefix
-			+ "You are not allowed to change the gamemode of other players";
+			+ "Вы не маеце дазволу на пераключэнне рэдыму гульні для іншых гульцоў.";
 
-	public static final String CurrentGamemodeIs = Prefix + "Your current gamemode is ";
-	public static final String GamemodeChangedTo = Prefix + "Gamemode changed to ";
-	public static final String NoPermissionToChangeTo = Prefix + "You are not allowed to change your gamemode to ";
+	public static final String CurrentGamemodeIs = Prefix + "Вы зараз у рэжыме ";
+	public static final String GamemodeChangedTo = Prefix + "Рэжым гульні зменены на ";
+	public static final String NoPermissionToChangeTo = Prefix + "Вы не маеце дазволу пераключаць рэжым гульні на ";
 	public static final String NoPermissionToChangeToForOther = Prefix
-			+ "You are not allowed to change this person's gamemode to ";
+			+ "Вы не маеце дазволу на пераключенне рэжыму гульні. ";
 
-	public static final String GamemodeCreative = ChatColor.GREEN + "Creative";
-	public static final String GamemodeSurvival = ChatColor.RED + "Survival";
-	public static final String GamemodeAdventure = ChatColor.GOLD + "Adventure";
-	public static final String GamemodeSpectator = ChatColor.BLUE + "Spectator";
+	public static final String GamemodeCreative = ChatColor.GREEN + "Творчы";
+	public static final String GamemodeSurvival = ChatColor.RED + "Выжыванне";
+	public static final String GamemodeAdventure = ChatColor.GOLD + "Прыгоды";
+	public static final String GamemodeSpectator = ChatColor.BLUE + "Назіральнік";
 }
